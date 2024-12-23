f869857169baf2d36b52fcb3ad658cdce94e7ae2
Erik <erik@example.com>
2024-12-23 20:13:34 +0100

    First commit

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+export interface CarScrappedData {
+    name: string;
+    price: string;
+    hp: string;
+    accel: string;
+    length: string;
+    weight: string;
+    trunk_space_seats: string;
+    trunk_space_no_seats: string;
+    fuel_tank: string;
+    url: string;
+}
