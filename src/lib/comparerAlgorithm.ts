bf7b767e173f7d866e6a8b5f5a7905fc67bbbb65
Erik <erik@example.com>
2025-01-03 14:05:30 +0100

    Finished color highlighting for current car attributes
    - Added color highlighting in Comparator component
    - Added more compare functions in `compareAlgorith.ts` (this functions should be reworked in the future)
    - Added more parser functions

--- src/lib/comparerAlgorithm.ts
+++ src/lib/comparerAlgorithm.ts
@@ -2,7 +2,57 @@ import type { CompItem } from "../types/types";
 
 export const compare = (items: CompItem[]): CompItem[] => {
   // 0-worse, 1-equal, 2-better
-  let result = [];
+  const i = 0;
+  comparePrice(items);
+  compareHp(items);
+  compareAccel(items);
+  compareWeight(items);
+  return items;
+};
+
+const compareWeight = (items: CompItem[]) => {
+    const i = 0;
+    if (items[i].weight === items[i + 1].weight) {
+        items[i].weight = 1;
+        items[i + 1].weight = 1;
+    } else if (items[i].weight > items[i + 1].weight) {
+        items[i].weight = 0;
+        items[i + 1].weight = 2;
+    } else {
+        items[i].weight = 2;
+        items[i + 1].weight = 0;
+    }
+}
+
+const compareAccel = (items: CompItem[]) => {
+    const i = 0;
+    if (items[i].accel === items[i + 1].accel) {
+        items[i].accel = 1;
+        items[i + 1].accel = 1;
+    } else if (items[i].accel > items[i + 1].accel) {
+        items[i].accel = 0;
+        items[i + 1].accel = 2;
+    } else {
+        items[i].accel = 2;
+        items[i + 1].accel = 0;
+    }
+}
+
+const compareHp = (items: CompItem[]) => {
+    const i = 0;
+    if (items[i].hp === items[i + 1].hp) {
+        items[i].hp = 1;
+        items[i + 1].hp = 1;
+    } else if (items[i].hp > items[i + 1].hp) {
+        items[i].hp = 2;
+        items[i + 1].hp = 0;
+    } else {
+        items[i].hp = 0;
+        items[i + 1].hp = 2;
+    }
+}
+
+const comparePrice = (items: CompItem[]) => {
   const i = 0;
   if (items[i].price === items[i + 1].price) {
     items[i].price = 1;
@@ -14,6 +64,4 @@ export const compare = (items: CompItem[]): CompItem[] => {
     items[i].price = 2;
     items[i + 1].price = 0;
   }
-  console.log(items);
-  return items;
 };
--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -1,9 +1,27 @@
 export const inputParser = (input: string) => {
   if (input.includes("€")) {
     return priceParser(input);
+  } else if (input.includes("CV")) {
+    return hpParser(input);
+  } else if (input.includes("s")) {
+    return accelParser(input);
+  } else if (input.includes("kg")) {
+    return weightParser(input);
   }
 };
 
 const priceParser = (price: string) => {
   return Number((price).replace("€", "").trim());
+};
+
+const hpParser = (hp: string) => {
+  return Number((hp.split(" "))[0].trim()); return
+};
+
+const accelParser = (accel: string) => {
+  return Number((accel).replace("s", "").trim());
+};
+
+const weightParser = (weight: string) => {
+  return Number((weight).replace("kg", "").trim());
 };
